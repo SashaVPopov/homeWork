dccc22e28e663aed6598ae838fb39d9864f820b7
SashaVPopov <154453977+SashaVPopov@users.noreply.github.com>
2024-01-26 22:39:54 +0300

    Update d10_demo.c

--- hw7/d10_demo.c
+++ hw7/d10_demo.c
@@ -29,37 +29,3 @@ int main(void)
 	is_prime(a,2) ? printf ("YES"):printf ("NO"); 
 	return 0;
 }
-
-=======
-/*
- * ep_d10_demo.c 
- * Ïðîâåðêà íà ïðîñòîòó
- * Äàíî íàòóðàëüíîå ÷èñëî n>=1. Ïðîâåðüòå, ÿâëÿåòñÿ ëè îíî, ïðîñòûì. 
- * Ïðîãðàììà äîëæíà âûâåñòè ñëîâî YES, åñëè ÷èñëî ïðîñòîå èëè NO â ïðîòèâíîì ñëó÷àå.
- * Íåîáõîäèìî ñîñòàâèòü ðåêóðñèâíóþ ôóíêöèþ è èñïîëüçîâàòü åå.
- * int is_prime (int n, int divider)
- * Äàííûå äëÿ âõîäà: Îäíî íàòóðàëüíîå ÷èñëî
- * Äàííûå íà âûõîä: YES èëè NO.
- */
-#include <stdio.h> 
-#include <locale.h> 
-int is_prime (int n, int divider)
-{
-	if (n==divider)
-		return 1;
-	if ((n>1)&&(n%divider != 0))
-		return is_prime(n,divider+1);
-	else 
-		return 0;
-}
-int main(void)
-{
-	setlocale (LC_ALL, "Rus");
-	int a;
-	printf("Ââåäèòå öåëîå ÷èñëî: ");
-	scanf ("%d", &a); 
-	is_prime(a,2) ? printf ("YES"):printf ("NO"); 
-	return 0;
-}
-
->>>>>>> 50d092cf354843ddc37e949cb799fe76c4eb1143
